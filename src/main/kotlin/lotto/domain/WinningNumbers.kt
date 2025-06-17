61745bddd14c0a87b10ed5aaf820dc82fe895681
minjong.kim <minjong.kim@deliveryhero.com>
2025-06-17 23:16:08 +0200

    refactor: add validation logic into static factory method

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -1,22 +1,26 @@
 package lotto.domain
 
-class WinningNumbers private constructor(winningNumbers: List<Number> = listOf()) {
-
-    init {
-        if (winningNumbers.size != winningNumbers.distinct().size) {
-            throw IllegalArgumentException("Winning numbers can not be duplicated")
-        }
-        if (winningNumbers.size != WINNING_NUMBER_COUNT) {
-            throw IllegalArgumentException("Winning numbers size must equal to $WINNING_NUMBER_COUNT")
-        }
-    }
+class WinningNumbers private constructor(winningNumbers: List<Number>) {
 
     companion object {
         const val WINNING_NUMBER_COUNT = 6
 
         fun fromInts(vararg numbers: Int): WinningNumbers {
+            require(numbers.distinct().size == WINNING_NUMBER_COUNT) {
+                "Winning numbers must be $WINNING_NUMBER_COUNT unique values"
+            }
+
             val numberList = numbers.map { Number(it) }
             return WinningNumbers(numberList)
+        }
+    }
+
+    init {
+        if (winningNumbers.size != winningNumbers.distinct().size) {
+            throw IllegalArgumentException("Winning numbers must not contain duplicates")
+        }
+        if (winningNumbers.size != WINNING_NUMBER_COUNT) {
+            throw IllegalArgumentException("Winning numbers size must equal to $WINNING_NUMBER_COUNT")
         }
     }
 }
