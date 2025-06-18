558b6233d29187893f22d6ab45031062601d4e1e
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-18 16:09:19 +0200

    feat: implement main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import lotto.domain.LottoGame
+import lotto.domain.Lottos
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    TODO("프로그램 구현")
+    val price = InputView.readPriceToBuy().toInt() // TODO: add validations
+    val lottos = Lottos(price / 1000) // TODO: remove magic number
+    OutputView.printLottoNumbers(lottos)
+
+    val winningNumbers = InputView.readWinningNumbers()
+    val bonusNumber = InputView.readBonusNumber()
+    val lottoGame = LottoGame(lottos, winningNumbers, bonusNumber)
+    OutputView.printGameResult(lottoGame)
 }
