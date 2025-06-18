01e1530c8d1b2f5dc30c145e12600d65dd1d2607
Gibeom Nam <rlqja0523@naver.com>
2025-06-19 01:52:54 +0200

    fix: translate text to Korean

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -4,14 +4,14 @@ private const val DEFAULT_DELIMITER = ","
 
 object InputView {
     fun readPurchaseAmount(): Int {
-        println("Please enter a purchase amount")
+        println("구입금액을 입력해 주세요.")
         val purchaseAmount = readlnOrNull() ?: throw IllegalArgumentException("You need to enter a purchase amount.")
         println()
         return purchaseAmount.toIntOrNull() ?: throw IllegalArgumentException("Purchase amount must be a number.")
     }
 
     fun readWinningNumbers(): List<Int> {
-        println("Please enter winner numbers.")
+        println("당첨 번호를 입력해 주세요.")
         val winningNumbers = readlnOrNull() ?: throw IllegalArgumentException("You need to enter winner numbers.")
         println()
         return winningNumbers.split(DEFAULT_DELIMITER)
@@ -20,7 +20,7 @@ object InputView {
     }
 
     fun readBonusNumber(): Int {
-        println("Please enter a bonus number.")
+        println("보너스 번호를 입력해 주세요.")
         val bonusNumber = readlnOrNull() ?: throw IllegalArgumentException("You need to enter a bonus number.")
         println()
         return bonusNumber.toIntOrNull() ?: throw IllegalArgumentException("Bonus number must be a number.")
