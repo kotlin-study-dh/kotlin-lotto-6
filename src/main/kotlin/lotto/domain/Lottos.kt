ec66ae66dee3288337233a740ec104d63d0d8b8d
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-18 14:13:47 +0200

    feat: add getters for `Lottos` size and elements

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -5,4 +5,8 @@ class Lottos(private val elements: List<Lotto>) {
     constructor(issueAmount: Int) : this(
         List(issueAmount) { Lotto() }
     )
+
+    fun size() = elements.size
+
+    fun getElements() = elements
 }
