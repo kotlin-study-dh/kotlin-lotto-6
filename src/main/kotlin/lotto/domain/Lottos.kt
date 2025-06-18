ee404b4731da39e8570871cc1c60692bc97e6cf8
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-18 20:36:45 +0200

    chore: use `require()` instead of `assert()`

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -5,13 +5,13 @@ private const val LOTTO_PRICE = 1000
 class Lottos(private val elements: List<Lotto>) {
 
     init {
-        assert(elements.isNotEmpty()) { "At least one lotto has to be bought." }
+        require(elements.isNotEmpty()) { "At least one lotto has to be bought." }
     }
 
     companion object {
         fun buy(price: Long): Lottos {
-            assert(price >= LOTTO_PRICE) { "Purchasing price must be $LOTTO_PRICE or greater." }
-            assert(price % LOTTO_PRICE == 0L) { "Purchasing price must be divisible by $LOTTO_PRICE." }
+            require(price >= LOTTO_PRICE) { "Purchasing price must be $LOTTO_PRICE or greater." }
+            require(price % LOTTO_PRICE == 0L) { "Purchasing price must be divisible by $LOTTO_PRICE." }
 
             val buyAmount = (price / LOTTO_PRICE).toInt()
             return Lottos(List(buyAmount) { Lotto() })
