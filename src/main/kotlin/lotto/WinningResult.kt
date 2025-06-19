b969abc20b5255fd3d015c6854618e7c681f2a4c
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-19 21:50:28 +0200

    feat: add WinningResult to store the count of rank

---
+++ src/main/kotlin/lotto/WinningResult.kt
@@ -0,0 +1,36 @@
+package lotto
+
+data class WinningResult(
+    private val resultsByRank: Map<LottoRank, Int>
+) {
+    constructor(
+        lottos: List<Lotto>,
+        winningNumbers: WinningNumbers
+    ) : this(
+        calculateResult(lottos, winningNumbers)
+    )
+
+    companion object {
+        fun calculateResult(lottos: List<Lotto>, winningNumbers: WinningNumbers): Map<LottoRank, Int> {
+            val winningResult = mutableMapOf<LottoRank, Int>().withDefault { 0 }
+            LottoRank.entries.forEach { rank ->
+                winningResult[rank] = 0
+            }
+
+            for (lotto in lottos) {
+                val matchingResultCnt = winningNumbers.calculateMatchingNumber(lotto)
+                val rank = when (matchingResultCnt) {
+                    6 -> LottoRank.SIX_MATCH
+                    5 -> if (winningNumbers.hasBonusNumber(lotto)) LottoRank.FIVE_BONUS_MATCH else LottoRank.FIVE_MATCH
+                    4 -> LottoRank.FOUR_MATCH
+                    3 -> LottoRank.THREE_MATCH
+                    else -> LottoRank.NONE_MATCH
+                }
+                winningResult[rank] = winningResult.getValue(rank) + 1
+            }
+            return winningResult
+        }
+    }
+
+    fun getByRank(lottoRank: LottoRank) = resultsByRank[lottoRank]
+}
