5340caaba37861b591328430445914d30d6267e1
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-19 21:48:14 +0200

    feat: add LottoRank

---
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -0,0 +1,13 @@
+package lotto
+
+enum class LottoRank(
+    val matchCounts: Int,
+    val prizeAmount: Int
+) {
+    SIX_MATCH(6, 2_000_000_000),
+    FIVE_BONUS_MATCH(5, 30_000_000),
+    FIVE_MATCH(5, 1_500_000),
+    FOUR_MATCH(4, 50_000),
+    THREE_MATCH(3, 5_000),
+    NONE_MATCH(0,0)
+}
