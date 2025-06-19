c025cf9bb39b054aa1e4870f79c934bededfd8f4
minjong.kim <minjong.kim@deliveryhero.com>
2025-06-19 21:09:47 +0200

    feat: implement console output logic

---
+++ src/main/kotlin/lotto/output/ConsoleOutput.kt
@@ -0,0 +1,28 @@
+package lotto.output
+
+import lotto.domain.Score
+import lotto.domain.number.Numbers
+
+class ConsoleOutput {
+
+    fun purchaseResponse(tickets: List<Numbers>) {
+        val primitiveNumbers =
+            tickets.map { guessingNumber -> guessingNumber.winningNumbers.map { number -> number.number } }
+        println("bought ${primitiveNumbers.size} of them.")
+        primitiveNumbers.map { it -> it.sorted() }
+            .forEach { println(it) }
+    }
+
+    fun winningStatisticsResponse(prizes: List<Score>) {
+        println("Winning Statistics")
+        println("3 matches: ${prizes.count { it == Score.FIFTH }}")
+        println("4 matches: ${prizes.count { it == Score.FOURTH }}")
+        println("5 matches: ${prizes.count { it == Score.THIRD }}")
+        println("5 matches & bonus match: ${prizes.count { it == Score.SECOND }}")
+        println("6 matches: ${prizes.count { it == Score.FIRST }}")
+    }
+
+    fun reteOfReturnResponse(rate: Double) {
+        println("total rate of return is $rate%!")
+    }
+}
