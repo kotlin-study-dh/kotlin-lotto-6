35fdf4488a731b5ad0625d3177534e321b48d45b
Kayoung Yoon <dev.kayoung@gmail.com>
2025-06-19 22:13:27 +0200

    fix: revenue must be calculated as a percentage

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -4,8 +4,11 @@ data class LottoResult(val prizes: List<Prize>, val revenue: Double) {
     companion object {
         fun of(prizes: List<Prize>, purchaseAmount: LottoPurchaseAmount): LottoResult {
             val totalReward = prizes.sumOf { it.reward }
-            val revenue = totalReward.toDouble() / purchaseAmount.value
+            val revenue = calculateRevenueInPercent(totalReward, purchaseAmount)
             return LottoResult(prizes, revenue)
         }
+
+        private fun calculateRevenueInPercent(totalReward: Long, purchaseAmount: LottoPurchaseAmount): Double =
+            totalReward.toDouble() / purchaseAmount.value * 100
     }
 }
