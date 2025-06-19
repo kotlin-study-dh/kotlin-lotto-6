3331e1d8baa2c08f2053b4bd0bdfe2001af04e86
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-19 21:50:24 +0200

    feat: add function to calculate matchingNumber and bonusNumber

--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -1,12 +1,22 @@
 package lotto
 
 data class WinningNumbers(
-    val numbers: Lotto,
+    val winningLotto: Lotto,
     val bonusNumber: Int
 ) {
     init {
-        require(!numbers.isContain(bonusNumber)) {
+        require(!winningLotto.isContain(bonusNumber)) {
             "[ERROR] LottoNumbers and bonusNumber are duplicated"
         }
     }
+
+    fun calculateMatchingNumber(lotto: Lotto): Int {
+        val matchingCnt = lotto.numbers.count { winningLotto.isContain(it) }
+        if (lotto.isContain(bonusNumber)) {
+            return matchingCnt + 1
+        }
+        return matchingCnt
+    }
+
+    fun hasBonusNumber(lotto: Lotto) = lotto.numbers.contains(bonusNumber)
 }
