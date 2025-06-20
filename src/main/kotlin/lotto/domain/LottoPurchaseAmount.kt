67ddb8e77f86a0eede06e4029d95217a4d79b7b5
Kayoung Yoon <dev.kayoung@gmail.com>
2025-06-20 15:28:34 +0200

    feat: Lotto purchase amount must be greater than zero

--- src/main/kotlin/lotto/domain/LottoPurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/LottoPurchaseAmount.kt
@@ -4,6 +4,8 @@ data class LottoPurchaseAmount(val value: Int) {
 
     init {
         require(value % PURCHASE_AMOUNT_UNIT == 0) { "Lotto purchase amount must be a multiple of $PURCHASE_AMOUNT_UNIT" }
+        require(value > 0) { "Lotto purchase amount must be greater than 0" }
+
     }
 
     fun getLottoAmount(): Int = value / PURCHASE_AMOUNT_UNIT
