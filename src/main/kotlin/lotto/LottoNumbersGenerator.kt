c84151b52552744e5f810c411a3f5cb9fbde1ece
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-20 13:25:08 +0200

    refactor: replace constanct to value

--- src/main/kotlin/lotto/LottoNumbersGenerator.kt
+++ src/main/kotlin/lotto/LottoNumbersGenerator.kt
@@ -1,8 +1,12 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.domain.Lotto
 
 object LottoNumbersGenerator {
 
-    fun generate(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+    fun generate(): List<Int> = Randoms.pickUniqueNumbersInRange(
+        Lotto.LOTTO_MIN_NUMBER, Lotto.LOTTO_MAX_NUMBER, Lotto.LOTTO_SIZE
+    )
+        .sorted()
 }
