ff1ef502638f2a109f4c63f606ef56ebc454376a
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-20 11:36:21 +0200

    feat: add input validation

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -4,24 +4,33 @@ import camp.nextstep.edu.missionutils.Console
 
 fun readPurchaseAmount(): Int {
     println("구입금액을 입력해 주세요.")
-
-    val amount = Console.readLine().toIntOrNull()
-        ?: throw IllegalArgumentException("[ERROR] Purchase amount must be natural number")
+    val amount = (Console.readLine().toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] Purchase amount must be natural number"))
     require((amount % 1000) == 0) {
         "[ERROR] Purchase amount must be unit of 1000won"
     }
+    require(amount > 0) {
+        "[ERROR] Purchase amount must be at least 1000won"
+    }
+
     return amount / 1000
 }
 
 fun readWinningNumbers(): List<Int> {
     println("\n당첨 번호를 입력해 주세요.")
-    return Console.readLine()
-        .split(",")
-        .filter { it.isNotBlank() }
-        .map { it.toInt() }
+    return Console.readLine().split(",")
+        .map {
+            it.toIntOrNull()
+                ?: throw IllegalArgumentException("[ERROR] Purchase amount must be natural number")
+        }
 }
 
 fun readBonusNumber(): Int {
     println("\n보너스 번호를 입력해 주세요.")
-    return Console.readLine().toInt()
+    return (Console.readLine().toIntOrNull()?.let { bonusNumber ->
+        require(bonusNumber in 1..45) {
+            "[ERROR] Bonus number must be must be between 1 and 45"
+        }
+        bonusNumber
+    } ?: throw IllegalArgumentException("[ERROR] Purchase amount must be natural number"))
 }
