fee033871a5448fb7b4b8f9eecc1afe117031921
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-20 12:21:36 +0200

    test: disabled unused test

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersI
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class ApplicationTest : NsTest() {
@@ -43,6 +44,7 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
+    @Disabled
     fun `예외 테스트`() {
         assertSimpleTest {
             runException("1000j")
