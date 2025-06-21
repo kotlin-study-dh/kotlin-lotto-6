4ba279a7f5a90a802822d6401730fa6b0d1b56cd
minjong.kim <minjong.kim@deliveryhero.com>
2025-06-21 20:55:14 +0200

    refactor: simplify iteration code redundancy

--- src/main/kotlin/lotto/domain/number/FixedNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/number/FixedNumberGenerator.kt
@@ -3,6 +3,6 @@ package lotto.domain.number
 class FixedNumberGenerator : NumberGenerator {
 
     override fun generate(count: Int): IntArray {
-        return IntArray(count) { it -> it + 1 }
+        return IntArray(count) { it + 1 }
     }
 }
