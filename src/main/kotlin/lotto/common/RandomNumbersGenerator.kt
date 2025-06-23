ef32d254b81dfef9f336b79289a1999cc088668f
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-23 14:47:12 +0200

    chore: use `apply` instead of `also`

--- src/main/kotlin/lotto/common/RandomNumbersGenerator.kt
+++ src/main/kotlin/lotto/common/RandomNumbersGenerator.kt
@@ -6,7 +6,7 @@ object RandomNumbersGenerator {
 
     fun generate(from: Int, to: Int, amount: Int): List<LottoNumber> {
         return (from..to).toMutableList()
-            .also { it.shuffle() }
+            .apply { shuffle() }
             .take(amount)
             .sorted()
             .map { LottoNumber(it) }
