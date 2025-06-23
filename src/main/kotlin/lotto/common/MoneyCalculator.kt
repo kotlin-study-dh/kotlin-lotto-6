788070e3d25af9380d5ac452c8f4b06054b533bb
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-23 13:44:43 +0200

    chore: apply expression body

--- src/main/kotlin/lotto/common/MoneyCalculator.kt
+++ src/main/kotlin/lotto/common/MoneyCalculator.kt
@@ -2,7 +2,6 @@ package lotto.common
 
 object MoneyCalculator {
 
-    fun calculateRateOfReturn(purchasedMoney: Long, earnedMoney: Long): Double {
-        return earnedMoney.toDouble() / purchasedMoney.toDouble() * 100.0
-    }
+    fun calculateRateOfReturn(purchasedMoney: Long, earnedMoney: Long): Double =
+        earnedMoney.toDouble() / purchasedMoney.toDouble() * 100.0
 }
